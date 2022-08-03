b7f8064b63f784d2545560cf93a46335245f8091
Travis Geers <armyman1992@hotmail.com>
2022-08-03 15:34:53 -0600

    user prompts created

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,31 @@ function writePassword() {
 
 }
 
+// User prompts when creating a password
+
+function createPassword() {
+  var password = "";
+  var length = Number(prompt("How many characters would you like you password to be?"));
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Password must be between 8 through 128 characters in length. How many characters would you like?"));
+
+  var uppercase = confirm("Would you like to use uppercase letters?");
+  var lowercase = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?")
+  var symbols = confirm("Would you like to use symbols?")
+
+  while (!uppercase && !lowercase && !numbers && !symbols) {
+    alert("You must select at least one character type!");
+    uppercase = confirm("Would you like to use uppercase letters?");
+    lowercase = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use symbols?");
+  }
+
+}
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
